dccb3180cf6871415111eb7615cd7a9e1d0a98f7
Malinowsk <juanrago2015@gmail.com>
2022-05-15 01:34:50 -0300

    Actualizacion responsive

--- js/captcha.js
+++ js/captcha.js
@@ -1,11 +1,14 @@
+let resultado; // variable para guardar el valor valido del captcha
+generarCaptcha();
+document.querySelector("#validar").addEventListener("click",validarCaptcha);//pongo a escuchar el evento para validar el captcha
+
+
 //genera el captcha para validar que es un humano
 function generarCaptcha(){
     let primer_numero = generarNumero(); // genero numero aleatorio
     let segundo_numero = generarNumero(); // genero numero aleatorio 
     document.querySelector("#prim_num").innerHTML = primer_numero; // agrego prim num en etiqueta span
     document.querySelector("#seg_num").innerHTML = segundo_numero; // agrego seg num en etiqueta span
-    let contenedor_captcha = document.querySelector("#captcha"); // selecciono el div 
-    contenedor_captcha.classList.toggle("oculto"); // le agrego o saco al div la clase para que aparesca o desaparesca
     resultado= primer_numero + segundo_numero; // guardo resultado valido del captcha
 }
 
@@ -19,20 +22,26 @@ function validarCaptcha(){
         let contenedor = document.querySelector("#captcha");
         contenedor.innerHTML="";
         contenedor.classList.remove("captcha");
-        informarResultado("Validación satisfactoria! ");
-        document.querySelector("#generar-captcha").removeEventListener("click",generarCaptcha); // borramos el evento de hacer click en el boton de generar captcha
+        document.querySelector("#oculta").checked = true; // confirma que el captcha fue validado para poder enviar el formulario
+        informarResultado(true);
     }
     else{
-        informarResultado("La validación se efectuó incorrectamente!");
+        informarResultado(false);
     }
 }
 
 //funcion que muestra por pantalla mensaje sobre la resolucion del captcha   
-function informarResultado(mensaje){ 
+function informarResultado(valido){ 
     let parrafo = document.querySelector("#info-resolucion");
-        parrafo.innerHTML = mensaje;
+    if (valido){
+        parrafo.classList.add("mensaje-valido");
+        parrafo.classList.remove("mensaje-invalido");
+        parrafo.innerHTML = "Validación satisfactoria!";
+    }
+    else
+    {
+        parrafo.classList.add("mensaje-invalido");
+        parrafo.innerHTML = "La validación se efectuó incorrectamente!";
+    }
 }
 
-document.querySelector("#generar-captcha").addEventListener("click",generarCaptcha); //pongo a escuchar el evento para generar el captcha
-document.querySelector("#validar").addEventListener("click",validarCaptcha);//pongo a escuchar el evento para validar el captcha
-let resultado; // variable para guardar el valor valido del captcha
